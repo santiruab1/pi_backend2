a5c500478080cbea9f98383cc6520728716ea62d
Diego Serna <juan.serna7836@unaula.edu.co>
2025-08-02 15:26:43 -0500

    feat: Add User entity with fields for ID, first name, surname, email, and password

--- src/main/java/com/example/pib2/model/entity/User.java
+++ src/main/java/com/example/pib2/model/entity/User.java
@@ -1,5 +1,26 @@
 package com.example.pib2.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    @Column(nullable = false, length = 100)
+    private String user_first_name;
+
+    @Column(nullable = false, length = 100)
+    private String user_sur_name;
+
+    @Column(nullable = false, length = 200,unique = true)
+    private String user_email;
+
+    @Column(nullable = false)
+    private String user_password;
 
 }
