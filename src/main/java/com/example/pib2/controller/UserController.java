e33d3bd1930785088e2913bf4b54ddf41e3540ac
Diego Serna <juan.serna7836@unaula.edu.co>
2025-08-30 14:25:56 -0500

    feat: Add UserController with createUser endpoint for user creation

--- src/main/java/com/example/pib2/controller/UserController.java
+++ src/main/java/com/example/pib2/controller/UserController.java
@@ -1,5 +1,27 @@
 package com.example.pib2.controller;
 
+import com.example.pib2.model.dto.UserDTO;
+import com.example.pib2.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
-    
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO) {
+        UserDTO createdUser = userService.createUser(userDTO);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
 }
